4d5b7998cc34c963d3886f2b5ea742a8d7435d50
Abid <abidroid1@gmail.com>
2025-05-04 11:28:30 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/ajk/uoajk_day5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ajk.uoajk_day5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/landing_screen.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Landing Screen"),
+        backgroundColor: Colors.deepOrange,
+        foregroundColor: Colors.white,
+      ),
+
+      body: ListView(
+        children: [
+
+          Card(
+
+            color: Colors.amber[100],
+            child: ListTile(
+              leading: Text('1'),
+              title: Text('Countries Flag API'),
+              onTap: (){},
+            ),
+          ),
+
+          Card(
+            color: Colors.amber[100],
+            child: ListTile(
+              leading: Text('2'),
+              title: Text('Products API'),
+              onTap: (){},
+            ),
+          ),
+        ],
+      ),
+
+    );
+  }
+}
